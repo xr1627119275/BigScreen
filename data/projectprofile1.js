ef01489461406282e37119c4abc11c41349092b7
xurui <1627119275@qq.com>
2022-01-07 19:36:54 +0800

    项目概况数据抽取

---
+++ data/projectprofile1.js
@@ -0,0 +1,149 @@
+var profileData = {
+    normal: [
+        { unit: 'km', num: '44.046'},
+        { num: '840876', unit: '万元'},
+        { unit: '天'}
+    ],
+    desc: [
+        '新都桥至波密段（以下简称中间段）站前工程正线长度681.481km，桥隧总长636.004km，桥隧比93.3%。其中隧道40座长545.913km，占比80.1%；桥梁68 座长90.811km，占比13.3%。新建车站15座，雅江、理塘、毛垭坝、巴塘、白玉、贡觉、昌都、邦达、洛隆为中间站，红龙、罗麦、则巴、扩达、夏里、多吉为越行站。',
+        'CZXZZQ-6标施工里程为DK1091+900-DK1052+080，长度44.046km，包含多木格隧道15695m、多吉隧道14445m（24446m）、瓤打曲大桥（主跨246m矮塔斜拉桥）、波堆藏布特大桥13326m、波密梁场307孔。'
+    ],
+    persons: [
+        {
+            "text": [
+                "姓名",
+                "业主代表",
+                "建设单位"
+            ],
+            "img": "./img/person_avator.png"
+        },
+        {
+            "text": [
+                "姓名",
+                "勘察负责人",
+                "勘察单位"
+            ],
+            "img": "./img/person_avator.png"
+        },
+        {
+            "text": [
+                "姓名",
+                "设计负责人",
+                "中铁第一勘察设计院集团"
+            ],
+            "img": "./img/person_avator.png"
+        },
+        {
+            "text": [
+                "姓名",
+                "项目经理",
+                "中铁四局集团"
+            ],
+            "img": "./img/person_avator.png"
+        },
+        {
+            "text": [
+                "姓名",
+                "监理工程师",
+                "中咨工程管理咨询"
+            ],
+            "img": "./img/person_avator.png"
+        },
+        {
+            "text": [
+                "姓名",
+                "勘察负责人",
+                "勘察单位"
+            ],
+            "img": "./img/person_avator.png"
+        },
+        {
+            "text": [
+                "姓名",
+                "设计负责人",
+                "中铁第一勘察设计院集团"
+            ],
+            "img": "./img/person_avator.png"
+        }
+    ],
+    milestone: [
+        {
+            "label": "已开工（天）：",
+            "value": ""
+        },
+        {
+            "label": "总工期（月）：",
+            "value": ""
+        },
+        {
+            "label": "已完成（万元）：",
+            "value": ""
+        },
+        {
+            "label": "合同造价（万元）：",
+            type: 'money',
+            "value": 840876
+        }
+    ],
+    milestoneTimeline: [
+        {
+            "status": "进行中",
+            "date": "2022-3-1",
+            "desc": "施工准备"
+        },
+        {
+            "status": "未开始",
+            "date": "2029-4-1",
+            "desc": "主体工程"
+        },
+        {
+            "status": "未开始",
+            "date": "2029-10-1",
+            "desc": "无砟隧道及沉降测"
+        },
+        {
+            "status": "未开始",
+            "date": "2031-8-30",
+            "desc": "铺轨工程"
+        },
+        {
+            "status": "未开始",
+            "date": "2032-5-8",
+            "desc": "站后及四电工程"
+        },
+        {
+            "status": "未开始",
+            "date": "2032-11-8",
+            "desc": "联调联试"
+        }
+    ],
+    somethingTimeline: [
+        {
+            "date": "2021-12-01",
+            "desc": "多木格隧道完工",
+            "img": "./img/something_demo1.png"
+        },
+        {
+            "date": "2021-12-01",
+            "desc": "多吉隧道完工",
+            "img": "./img/something_demo1.png"
+        },
+        {
+            "date": "2021-12-01",
+            "desc": "瓤打曲大桥完工",
+            "img": "./img/something_demo1.png"
+        },
+        {
+            "date": "2021-12-01",
+            "desc": "波堆藏布特大桥完工",
+            "img": "./img/something_demo1.png"
+        },
+        {
+            "date": "2021-12-01",
+            "desc": "波密梁场307孔完工",
+            "img": "./img/something_demo1.png"
+        }
+    ],
+    startDate: '2021-12-1',
+    endDate: '2032-11-8',
+}
