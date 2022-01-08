8b050b1d080133442139043f5f29f884099c4413
xurui <1627119275@qq.com>
2022-01-08 18:02:24 +0800

    添加巢马数据

---
+++ data/projectprofile2.js
@@ -0,0 +1,177 @@
+var profileData = {
+    normal: [
+        { unit: 'km', num: '3.574'},
+        { num: '110421', unit: '万元'},
+        { unit: '天'}
+    ],
+    desc: [
+        '中铁四局巢马铁路CMSG-3标二分部位于马鞍山市当涂县，正线里程范围DK57+329.218 (长江大桥F1#墩中心)～DK60+903.805 (跨205国道特大桥63#墩)，正线线路长3574m，马鞍山南动走线特大桥线路长640m；马鞍山南安全中桥线路长65.395m。马鞍山南存车场及维修工区线路长1252m。 副汊航道桥主跨采用392m双塔钢桁梁斜拉桥，全桥孔跨布置为：(56+168+392+168+56)双塔钢桁梁斜拉桥，桥长：840m。其中F3、F4主塔采用A型混凝土结构，塔高170m，F2、F5辅助墩采用横向门式墩，F1、F6号边墩为双层（下层为铁路桥墩采用横向门式墩、上层为公路框架墩）。'
+    ],
+    persons: [
+        {
+            "text": [
+                "郭小楠",
+                "项目经理",
+                "巢马二分部"
+            ],
+            "img": "https://gitee.com/xr13140/img/raw/master/20220108170434.png"
+        },
+        {
+            "text": [
+                "王兵",
+                "项目书记",
+                "巢马二分部"
+            ],
+            "img": "https://gitee.com/xr13140/img/raw/master/20220108170631.png"
+        },
+        {
+            "text": [
+                "高志军",
+                "项目总工",
+                "巢马二分部"
+            ],
+            "img": "https://gitee.com/xr13140/img/raw/master/20220108170721.png"
+        },
+        {
+            "text": [
+                "陈春杰",
+                "副经理",
+                "巢马二分部"
+            ],
+            "img": "https://gitee.com/xr13140/img/raw/master/20220108170811.png"
+        },
+        {
+            "text": [
+                "鲁勇",
+                "经理助理",
+                "巢马二分部"
+            ],
+            "img": "https://gitee.com/xr13140/img/raw/master/20220108170848.png"
+        },
+        {
+            "text": [
+                "成泓池",
+                "副总工",
+                "巢马二分部"
+            ],
+            "img": "https://gitee.com/xr13140/img/raw/master/20220108170928.png"
+        },
+        {
+            "text": [
+                "辛昌祥",
+                "工程部长",
+                "巢马二分部"
+            ],
+            "img": "https://gitee.com/xr13140/img/raw/master/20220108171002.png"
+        },
+        {
+            "text": [
+                "王铮",
+                "物机部长",
+                "巢马二分部"
+            ],
+            "img": "https://gitee.com/xr13140/img/raw/master/20220108171040.png"
+        },
+        {
+            "text": [
+                "高伟",
+                "安质部长",
+                "巢马二分部"
+            ],
+            "img": "https://gitee.com/xr13140/img/raw/master/20220108171257.png"
+        },
+        {
+            "text": [
+                "李友强",
+                "办公室主任",
+                "巢马二分部"
+            ],
+            "img": "https://gitee.com/xr13140/img/raw/master/20220108171339.png"
+        },
+    ],
+    milestone: [
+        {
+            "label": "已开工（天）：",
+            "value": ""
+        },
+        {
+            "label": "总工期（月）：",
+            "value": ""
+        },
+        {
+            "label": "已完成（万元）：",
+            "value": ""
+        },
+        {
+            "label": "合同造价（万元）：",
+            type: 'money',
+            "value": 110421
+        }
+    ],
+    milestoneTimeline: [
+        {
+            "status": "已完成",
+            "date": "2021/12/22",
+            "desc": "F3/F4墩桩基"
+        },
+        {
+            "status": "进行中",
+            "date": "2022/7/8",
+            "desc": "F3#墩承台"
+        },
+        {
+            "status": "进行中",
+            "date": "2022/6/15",
+            "desc": "F4#墩承台"
+        },
+        {
+            "status": "未开始",
+            "date": "2023/10/17",
+            "desc": "F4#主塔"
+        }
+    ],
+    somethingTimeline: [
+        {
+            "date": "2020年1月30",
+            "desc": "局施工调查、管理交底、管理策划研讨",
+            "img": "https://s2.loli.net/2022/01/08/CbMg6Eh5nNdYHVy.png"
+        },
+        {
+            "date": "2020年2月8日",
+            "desc": "重点施工方案研讨会",
+            "img": "https://s2.loli.net/2022/01/08/4yQdj3xm7ouhr2N.png"
+        },
+        {
+            "date": "2021年3月11日",
+            "desc": "重点施工方案研讨会",
+            "img": "https://s2.loli.net/2022/01/08/YgR9dNSq3yn762A.png"
+        },
+        {
+            "date": "2021年5月21日",
+            "desc": "实施性施工组织设计评审会",
+            "img": "https://s2.loli.net/2022/01/08/fCYr5xl9eq41Tug.png"
+        },
+        {
+            "date": "2021年6月15日",
+            "desc": "重大方案现场推进会",
+            "img": "https://s2.loli.net/2022/01/08/fgQElRwNKxeLsSA.png"
+        },
+        {
+            "date": "2021年7月29日",
+            "desc": "钻孔桩及承台施工方案评审会",
+            "img": "https://s2.loli.net/2022/01/08/X8wDClhKYtqEu7Z.png"
+        },
+        {
+            "date": "2021年8月12日",
+            "desc": "施工管理调研",
+            "img": "https://s2.loli.net/2022/01/08/4UTfN6y3VscmHJ9.png"
+        },
+        {
+            "date": "2021年9月8日",
+            "desc": "重大方案研讨",
+            "img": "https://s2.loli.net/2022/01/08/bic3vOCZGExw9qF.png"
+        }
+    ],
+    startDate: '2021/5/16',
+    endDate: '2026/1/10',
+}
